9f6caa9ff41780cfa1a9735a02295254532c12a5
Sreejith K U <sreejithku42@gmail.com>
2021-03-18 08:57:41 +0530

    merge contact and about page

--- smoc-web-app/src/content/ContactUs/ContactUsComponent.jsx
+++ smoc-web-app/src/content/ContactUs/ContactUsComponent.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import "./ContactUsComponent.css";
+import ContactHeader from "../../assests/ContactusImage.svg";
 
 const ContactUsComponent = () => {
   return (
-    <section>
-      <contactbg>
-        <h1>Contact us</h1>
-      </contactbg>
-      <bgsec></bgsec>
-    </section>
+    <div className="contact">
+      <div className="contact-header">
+        <div className="contact-header-title">Media contact</div>
+        <div className="contact-header-image">
+          <img src={ContactHeader} alt="contactheader"></img>
+        </div>
+      </div>
+      <div className="contact-section"></div>
+    </div>
   );
 };
 
