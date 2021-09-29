6827c9c20ed2f0d64a10f9ec1fe56682551845bc
Sreejith K U <41893973+Sreejith42@users.noreply.github.com>
2021-09-29 21:39:54 +0530

    changed color of header text and fixed footer issue

--- smoc-web-app/src/content/AboutUs/AboutUsComponent.jsx
+++ smoc-web-app/src/content/AboutUs/AboutUsComponent.jsx
@@ -95,6 +95,7 @@ const AboutUsComponent = () => {
                 backgroundColor: "white",
                 borderRadius: "10px",
                 marginTop: "50px",
+                marginBottom:"40px",
                 boxShadow:
                   "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
               }}
